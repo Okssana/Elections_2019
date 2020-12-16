c8e7c7b7e055dcac5c684b095e4e05f2cb072f7f
Okssana <40266071+Okssana@users.noreply.github.com>
2020-12-16 17:23:17 +0200

    Update and rename cvk_results to cvk_results.R

--- cvk_results.R
+++ cvk_results.R
@@ -6,11 +6,14 @@ library(xml2)
 #Download files from the Central Comission web-site
 
 registered_major <- for (n in c(11:40, 45:52, 57:60, 62:103, 105:107, 112:223, 226)){            #Numbers of voting districts, totally 198
+
   url_2019 <- paste0("https://www.cvk.gov.ua/pls/vnd2019/wp033pt001f01=919pf7331=", n ,".html")
   
   if(url.exists(url_2019) == TRUE){
     
-    results_2019 <- read_html(url_2019)%>% html_table(fill = TRUE)
+    results_2019 <- read_html(url_2019)%>% 
+    html_table(fill = TRUE)
+    
     results_2019[[6]]%>%
       data.frame()
     
